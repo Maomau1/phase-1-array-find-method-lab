266f9d45e7005c88d39f110a13483081e3987ac8
maurice diong <mdiong00@gmail.com>
2022-10-08 12:12:21 -0400

    commit tree b7fbf3859eb112b9f4d9cb98901979069c2ac5d5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function winning(element) {
+    return (element.result==="W")
+}
+
+function superbowlWin(array){
+    if (array.find(winning) ===undefined) {
+        return array.find(winning)
+    }
+    else return array.find(winning).year
+}
