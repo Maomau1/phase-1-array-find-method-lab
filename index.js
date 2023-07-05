49135507bc558955de6ffd9ce3353cc4cbcc62ce
maurice diong <mdiong00@gmail.com>
2023-07-05 03:09:02 -0400

    find method lab completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(array) {
+    if (array.find(item => item.result === "W")) {
+        return array.find(item => item.result === "W").year
+    }
+    else {
+        return undefined
+    }
+}
